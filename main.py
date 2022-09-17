8002a37178ec93e167277d06237ce5a308f351d9
atrox3d <atrox3d@gmail.com>
2022-09-17 06:36:53 +0200

    205. Inserting Records to a Table

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 """
 CREATE TABLE "Seat" (
-	"seat_id"	TEXT,
-	"taken"	INTEGER,
-	"price"	REAL
+    "seat_id"   TEXT,
+    "taken" INTEGER,
+    "price"	REAL
 )
 """
 
 import sqlite3
 
-connection = sqlite3.connect('cinema.db')
-connection.execute("""
-CREATE TABLE "Seat" (
-	"seat_id"	TEXT,
-	"taken"	INTEGER,
-	"price"	REAL
-)
-""")
-connection.commit()
-connection.close()
 
+def create_table():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    CREATE TABLE "Seat" (
+        "seat_id"	TEXT,
+        "taken"	INTEGER,
+        "price"	REAL
+    )
+    """)
+    connection.commit()
+    connection.close()
+
+
+def insert_record():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    INSERT INTO "Seat" ("seat_id", "taken", "price") VALUES ("A1", "0", "90"), ("A2", "1", "100"), ("A3", "0", "80")
+    """)
+    connection.commit()
+    connection.close()
+
+insert_record()
