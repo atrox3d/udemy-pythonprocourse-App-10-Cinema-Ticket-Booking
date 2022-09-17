04292fe354c60c35ad6ba7c12898c094309154fe
atrox3d <atrox3d@gmail.com>
2022-09-17 07:46:21 +0200

    RESET project after tutorial

---
+++ slqlite-tutorial.py
@@ -0,0 +1,91 @@
+"""
+CREATE TABLE "Seat" (
+    "seat_id"   TEXT,
+    "taken" INTEGER,
+    "price"	REAL
+)
+"""
+
+import sqlite3
+
+
+def create_table():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    CREATE TABLE "Seat" (
+        "seat_id"	TEXT,
+        "taken"	INTEGER,
+        "price"	REAL
+    )
+    """)
+    connection.commit()
+    connection.close()
+
+
+def insert_record():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    INSERT INTO "Seat" ("seat_id", "taken", "price") VALUES ("A1", 0, 90), ("A2", "1", "100"), ("A3", "0", "80")
+    """)
+    connection.commit()
+    connection.close()
+
+
+def select_all():
+    connection = sqlite3.connect('cinema.db')
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT * FROM "Seat"
+    """)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+
+def select_specific_columns():
+    connection = sqlite3.connect('cinema.db')
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT "seat_id", "price" FROM "Seat"
+    """)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+
+def select_with_condition():
+    connection = sqlite3.connect('cinema.db')
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT "seat_id", "price" FROM "Seat" WHERE "price" > 80
+    """)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+
+def update_value(occupied, seat_id):
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    UPDATE "Seat" SET "taken"=? WHERE "seat_id"=?
+    """, [occupied, seat_id])
+    connection.commit()
+    connection.close()
+
+
+def delete_record():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    DELETE FROM "Seat" WHERE "seat_id"="A3"
+    """)
+    connection.commit()
+    connection.close()
+
+
+print(f'{"select_all":>25} | ', select_all())
+print(f'{"select_specific_columns":>25} | ', select_specific_columns())
+print(f'{"select_with_condition":>25} | ', select_with_condition())
+
+update_value(1, "A2")
+delete_record()
+print(f'{"select_all":>25} | ', select_all())
