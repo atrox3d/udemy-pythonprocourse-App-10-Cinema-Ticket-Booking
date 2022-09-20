aba344dc9f03e70084c595d5266e7b7841ebacb8
atrox3d <atrox3d@gmail.com>
2022-09-20 13:48:59 +0200

    214. The Complete Code - User.buy()

--- user.py
+++ user.py
@@ -1,7 +1,23 @@
+from seat import Seat
+from card import Card
+from ticket import Ticket
+
+
 class User:
 
     def __init__(self, name):
         self.name = name
 
-    def buy(self, seat, card):
-        pass
+    def buy(self, seat: Seat, card: Card):
+        if seat.is_free():
+            price = seat.get_price()
+            if card.validate(price):
+                seat.occupy()
+                ticket = Ticket(self, price, seat.seat_id)
+                ticket.to_pdf()
+                return "Purchase succesful!"
+            else:
+                return "There was a problem with your card!"
+        else:
+            return "Seat is taken"
+
