7dd1cabac2682062dc07e47af8cd069ec6e58a43
atrox3d <atrox3d@gmail.com>
2022-09-20 14:11:08 +0200

    214. The Complete Code - Seat.get_price()

--- seat.py
+++ seat.py
@@ -9,7 +9,14 @@ class Seat:
         self.seat_id = seat_id
 
     def get_price(self):
-        pass
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "price" from "Seat" WHERE "seat_id" = ?
+        """, [self.seat_id])
+        result = cursor.fetchall()
+        price = result[0][0]
+        return price
 
     def is_free(self):
         connection = sqlite3.connect(self.database)
@@ -29,3 +36,4 @@ class Seat:
 if __name__ == '__main__':
     seat = Seat('A1')
     print(seat.is_free())
+    print(seat.get_price())
