9d0fe477c7330c4e968f7899642ce418752848e8
atrox3d <atrox3d@gmail.com>
2022-09-20 14:39:44 +0200

    214. The Complete Code - Ticket.to_pdf()

--- ticket.py
+++ ticket.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+from fpdf import FPDF
+
 
 class Ticket:
 
@@ -13,4 +15,36 @@ class Ticket:
         )
 
     def to_pdf(self):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family='Times', style='B', size=24)
+        pdf.cell(w=0, h=80, txt='your digital ticket', border=1, ln=1, align='C')
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt='Name: ', border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.user.name, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=1, ln=1)
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt='Ticket ID: ', border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.id, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=1, ln=1)
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt='Price : ', border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.price, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=1, ln=1)
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt='Seat Number: ', border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.seat_number, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=1, ln=1)
+
+        pdf.output('sample.pdf', dest='F')
+
+
