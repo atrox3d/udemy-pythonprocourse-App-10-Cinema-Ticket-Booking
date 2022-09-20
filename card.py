2e413faa9e01597956af0e5dc074a90b801e1e17
atrox3d <atrox3d@gmail.com>
2022-09-20 14:21:19 +0200

    214. The Complete Code - Card.validate()

--- card.py
+++ card.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class Card:
 
     database = 'banking.db'
@@ -9,4 +12,19 @@ class Card:
         self.holder = holder
 
     def validate(self, price):
-        pass
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "balance" from "Card" WHERE "number" = ? and "cvc" = ?
+        """, [self.number, self.cvc])
+        result = cursor.fetchall()
+
+        if result:
+            balance = result[0][0]
+            if balance >= price:
+                connection.execute("""
+                UPDATE "Card" SET "balance" = ? WHERE "number" = ? and "cvc" = ?
+                """, [balance - price, self.number, self.cvc])
+                connection.commit()
+                connection.close()
+                return True
