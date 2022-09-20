355049a60ada79b2a03bf7e190865f1a1c224043
atrox3d <atrox3d@gmail.com>
2022-09-20 15:54:53 +0200

    apply db.*

--- classes/card.py
+++ classes/card.py
@@ -1,30 +1,26 @@
-import sqlite3
+from classes.db import DB
 
 
 class Card:
 
-    database = 'data/banking.db'
+    database = '../data/banking.db'
 
     def __init__(self, type, number, cvc, holder):
         self.type = type
         self.number = number
         self.cvc = cvc
         self.holder = holder
+        self.db = DB(self.database)
 
     def validate(self, price):
-        connection = sqlite3.connect(self.database)
-        cursor = connection.cursor()
-        cursor.execute("""
-        SELECT "balance" from "Card" WHERE "number" = ? and "cvc" = ?
-        """, [self.number, self.cvc])
-        result = cursor.fetchall()
-
+        result = self.db.select('SELECT "balance" from "Card" WHERE "number" = ? and "cvc" = ?', self.number, self.cvc)
         if result:
             balance = result[0][0]
             if balance >= price:
-                connection.execute("""
-                UPDATE "Card" SET "balance" = ? WHERE "number" = ? and "cvc" = ?
-                """, [balance - price, self.number, self.cvc])
-                connection.commit()
-                connection.close()
+                self.db.update('UPDATE "Card" SET "balance" = ? WHERE "number" = ? and "cvc" = ?', balance - price, self.number, self.cvc)
                 return True
+
+
+if __name__ == '__main__':
+    card = Card('Visa', '12345678', '123', 'John Smith')
+    card.validate(4400)
--- classes/seat.py
+++ classes/seat.py
@@ -1,4 +1,3 @@
-import sqlite3
 from classes.db import DB
 
 
